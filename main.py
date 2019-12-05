30120ae66d6870cf93f36024e16beb92ec63e1ef
Daniel Brown <brown.daniel55@yahoo.com>
2019-12-05 15:01:51 -0500

    Added !quit and !load functions

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ def main():
     env = Environment()
     while True:
         str = input()
-        toke_str = par.tokenize(str)
-        syntax_tree = par.parse(toke_str)
-        #insert_quote(syntax_tree)
-        out = ev.eval(syntax_tree, env)
-        if out is not None:
-            #need to check is return value is is a list
-            #if it is, need to recursivley turn the list into a symbol instead of returning a list
-            print(out)
+        if str == "!quit":
+            print("Quitting lisp interpreter")
+            break
+
+        if len(str) >= 5 and str[0:5] == "!load":
+            print("loading file")
+            filename = str[6:]
+            with open(filename) as f:
+                for line in f:
+                    ev.eval(par.parse(par.tokenize(line)), env)
+
+        else:
+            toke_str = par.tokenize(str)
+            syntax_tree = par.parse(toke_str)
+            #insert_quote(syntax_tree)
+            out = ev.eval(syntax_tree, env)
+            if out is not None:
+                #need to check is return value is is a list
+                #if it is, need to recursivley turn the list into a symbol instead of returning a list
+                print(out)
 
 main()
